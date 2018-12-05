89b04bd438935d36d42f2a516693ae82ce9d8b46
Shinuey <kevin.kleemann@outlook.de>
2018-12-05 18:06:27 +0100

    Initial commit

---
+++ Library/Collab/Base/Assets/HansWoerstController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HansWoerstController : MonoBehaviour
+{
+    /// <summary>
+    /// Halo zusammen
+    /// </summary>
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Init Woerst");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        while (true)
+        {
+
+        }
+    }
+}
